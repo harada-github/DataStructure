a85026b2a1ceeda3ee124126167d21c6de717a37
harada <doesn.t.mind._l_.627@icloud.com>
2021-10-06 13:41:16 +0900

    Queueプロジェクト完成

--- Queue/main.cpp
+++ Queue/main.cpp
@@ -7,6 +7,7 @@
 //
 //============================================================
 #include "list.h"
+#include "queue.h"
 
 
 //============================================================
@@ -17,5 +18,33 @@
 //------------------------------------------------------------
 int main(void)
 {
-	
+	// 要素を追加
+	Queue::Enqueue(0, "Tanaka");
+	Queue::Enqueue(1, "Satou");
+	Queue::Enqueue(2, "Kobayashi");
+	Queue::Enqueue(3, "Kishida");
+	Queue::Enqueue(4, "Shimamura");
+	Queue::Enqueue(5, "Suga");
+	// データを一覧表示
+	Queue::Disp();
+	// 改行用
+	std::cout << "" << endl;
+
+
+	// １回目
+	// 要素を取り出す
+	Queue::Dequeue();
+	std::cout << "" << endl;
+	// データを再度一覧表示
+	Queue::Disp();
+	// 改行用
+	std::cout << "" << endl;
+
+
+	// ２回目
+	// 要素を取り出す
+	Queue::Dequeue();
+	std::cout << "" << endl;
+	// データを再度一覧表示
+	Queue::Disp();
 }
